8da45ab088b673061a3d4dff2dd7d06a1f758f82
Leszek Mazur <valutcizen@gmail.com>
2025-10-12 15:31:50 +0200

    Added enum for user roles

---
+++ src/common/models/auth.models.ts
@@ -0,0 +1,8 @@
+export enum UserRole {
+  Guest = 'Guest',
+  Member = 'Member',
+  Coordinator = 'Coordinator',
+  Confirmator = 'Confirmator',
+  ShootingRangeAdministrator = 'Shooting Range Administrator',
+  ClubCommunityAdministrator = 'Club/Community Administrator',
+}
